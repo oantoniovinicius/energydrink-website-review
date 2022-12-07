61aed4ff999fb12aaa7734bf54c292991dc04b47
oantoniovinicius <devantoniovinicius@gmail.com>
2022-12-06 23:13:18 -0300

    creating a slider

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$('.first-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow:1,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplayspeed: 2000
+  });
